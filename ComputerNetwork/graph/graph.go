d6a058a084f22b989b92e3bba6ca6a4c58c9ca37
Danila Bespyatov <bespiatov.ds@dozen.mephi.ru>
2024-07-17 11:01:59 +0300

    trying slices

--- ComputerNetwork/graph/graph.go
+++ ComputerNetwork/graph/graph.go
@@ -1,7 +1,13 @@
 package graph
 
+import "fmt"
+
 var D = 4
 
 func AddBDS(x, y int) int {
 	return x + y
+}
+
+func PrintSlice(s []int) {
+	fmt.Printf("len = %d cap = %d %v", len(s), cap(s), s)	
 }
