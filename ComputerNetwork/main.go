43d66e1a0fe70dadee45182bc3849e4b289524f1
Danila Bespyatov <bespiatov.ds@dozen.mephi.ru>
2024-07-17 14:49:28 +0300

    want to do create_graph func

--- ComputerNetwork/main.go
+++ ComputerNetwork/main.go
@@ -7,24 +7,6 @@ import (
 )
 
 func main() {
-	a := make([]int, 5)	
-	graph.PrintSlice("a", a)
-
-	b := make([]int, 0, 5)
-	graph.PrintSlice("b", b)
-
-	c := a[:2]
-	graph.PrintSlice("c", c)
-	c[0] = 1
-	c[1] = 2
-	graph.PrintSlice("new c", c)
-	graph.PrintSlice("new a", a)
-
-	d := b[:2]
-	graph.PrintSlice("d", d)
-
-	e := c[2:5]
-	graph.PrintSlice("e", e)
-
-	
+	var graph Graph
+	graph = graph.Create_graph()
 }
