ac20e306c142a5cec5be2d6c31776abec09990f4
Danila Bespyatov <bespiatov.ds@dozen.mephi.ru>
2024-07-17 11:39:13 +0300

    playing with slices

--- ComputerNetwork/main.go
+++ ComputerNetwork/main.go
@@ -7,6 +7,24 @@ import (
 )
 
 func main() {
-	s := []int{2, 3, 5, 7, 11, 13}
-	graph.PrintSlice(s)
+	a := make([]int, 5)	
+	graph.PrintSlice("a", a)
+
+	b := make([]int, 0, 5)
+	graph.PrintSlice("b", b)
+
+	c := a[:2]
+	graph.PrintSlice("c", c)
+	c[0] = 1
+	c[1] = 2
+	graph.PrintSlice("new c", c)
+	graph.PrintSlice("new a", a)
+
+	d := b[:2]
+	graph.PrintSlice("d", d)
+
+	e := c[2:5]
+	graph.PrintSlice("e", e)
+
+	
 }
