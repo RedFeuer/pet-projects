d6b7302e4f1722f24a4cea7efae62d3c70b5b761
Danila Bespyatov <bespiatov.ds@dozen.mephi.ru>
2024-07-19 10:56:21 +0300

    added D1, create_vertex and initialize_node funcs

--- ComputerNetwork/main/main.go
+++ ComputerNetwork/main/main.go
@@ -51,39 +51,77 @@ func Create_graph() *Graph {
 	}
 }
 
+func Initialize_node(new_vertex *Vertex) *Node {
+	var new_node *Node
+	new_node.Vertex = new_vertex
+	return new_node
+}
+
+func Create_vertex(comp string, port uint) *Vertex {
+	var new_vertex *Vertex
+	new_vertex.Comp = comp
+	new_vertex.Port = port
+	return new_vertex
+}
+
 func D1_Insert_Vertex(graph *Graph) {
-	fmt.Printf("Hello, main, from dialog!\n")
+	fmt.Printf("Enter unique computer name: ")
+	var comp string
+	fmt.Scan(&comp)
+	if len(comp) == 0 {
+		/*ПОДУМАТЬ КАК СДЕЛАТЬ ОБРАБОТКУ ОШИБОК КРАСИВЕЙ*/
+		fmt.Printf("ERROR: Computer-name is empty\n");
+		return
+	}
+	fmt.Printf("Enter number of port for computer %s: ", comp)
+	var port uint
+	fmt.Scanf("%u\n", &port)
+
+	/*ПРОВЕРКА: СУЩЕСТВУЕТ ЛИ ЭЛЕМЕНТ С ТАКИМ КЛЮЧОМ В ГРАФЕ*/
+	// var elem *Node
+	// var ok bool
+	new_vertex := Create_vertex(comp, port)
+	new_node := Initialize_node(new_vertex)
+	graph.Table[comp] = new_node
 }
 
 func main() {
 	/*создаем граф*/
 	graph := Create_graph()
 
-	D1_Insert_Vertex(graph)
+	var flag int = 1
+	for flag == 1 {
+		var choice int
+		switch choice {
+			default : // ВЫХОД ИЗ ПРОГРАММЫ
+				flag = 0
+			case 1 :
+				D1_Insert_Vertex(graph)
+		} 
+	}
+	// for i := 0; i < 1000; i++ {
+    //     compName := fmt.Sprintf("computer%d", i)
+    //     node := &Node{
+    //         Vertex: &Vertex{
+    //             Comp:      compName,
+    //             Port:      uint(8000 + i),
+    //             Color:     0,
+    //             Path_size:  0,
+    //             Component: i+1,
+    //         },
+    //     }
+    //     graph.Table[compName] = node
+    // }
 
-	for i := 0; i < 1000; i++ {
-        compName := fmt.Sprintf("computer%d", i)
-        node := &Node{
-            Vertex: &Vertex{
-                Comp:      compName,
-                Port:      uint(8000 + i),
-                Color:     0,
-                Path_size:  0,
-                Component: i+1,
-            },
-        }
-        graph.Table[compName] = node
-    }
+    // // Проверка количества вершин в графе
+    // fmt.Println("Number of vertices in the graph:", len(graph.Table))
 
-    // Проверка количества вершин в графе
-    fmt.Println("Number of vertices in the graph:", len(graph.Table))
-
-    // Доступ к одной из вершин
-    if n, exists := graph.Table["computer500"]; exists {
-        // Работа с найденной вершиной
-        fmt.Println("Vertex:", n.Vertex.Comp)
-        fmt.Println("Port:", n.Vertex.Port)
-    } else {
-        fmt.Println("Vertex not found")
-    }
+    // // Доступ к одной из вершин
+    // if n, exists := graph.Table["computer500"]; exists {
+    //     // Работа с найденной вершиной
+    //     fmt.Println("Vertex:", n.Vertex.Comp)
+    //     fmt.Println("Port:", n.Vertex.Port)
+    // } else {
+    //     fmt.Println("Vertex not found")
+    // }
 }
